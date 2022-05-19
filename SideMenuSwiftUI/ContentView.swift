af0a8255bdb4b9a73d30627422e810291524352e
Berat Yavuz <beratttyavuzz@gmail.com>
2022-05-19 13:05:09 +0300

    Initial Commit

---
+++ SideMenuSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideMenuSwiftUI
+//
+//  Created by Berat Yavuz on 19.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideMenuSwiftUI/SideMenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftUIApp.swift
+//  SideMenuSwiftUI
+//
+//  Created by Berat Yavuz on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
